50e329f10faffc90e1e360a2de08b67c684021e3
Joseph Macalinao <jhmac121101@gmail.com>
2022-12-14 11:54:07 -0700

    final before return

--- Intro.cpp
+++ Intro.cpp
@@ -55,36 +55,52 @@ Paladin *createPaladin(std::string newName, int newHealth, int newAttack){ // cr
 
 void create(){
     int i = 1; // used for later while loop
-    std::fstream name_create; // name grab from cin but with characters
+    std::string name_create; // name grab from cin but with characters
     char buffer[100]; // buffer for name grab
     //struct Character *create = (Character*)malloc(sizeof(Character)); //honestly don't need it
     std::cout<< "Please enter your name:";
-    name_create.getline( buffer, sizeof buffer ); // actually grabbing line
+    std::getline(std::cin, name_create); // actually grabbing line
     
     while(i > 0){ // supposed to break when a valid class is chosen
                 // class options: Warrior, Mage, Paladin
 
         std::string s;
-        std::getline(std::cin, s);
-        const char *warr = "warrior";
+        
+        const char *warr = "warrior"; // need to do this for strcmp for some reason
+        const char *mag = "mage";        //  |
+        const char *pal = "paladin";     //  |
 
         std::cout << "Would you like to be:" << std::endl << //asking for player choice of class
         "Warror: W" << std::endl <<
         "Mage: M" << std::endl <<
-        "Paladin: P";
-
-        const char* choice = s.c_str();
+        "Paladin: P" << std::endl;
+        std::getline(std::cin, s);
+        const char* choice = s.c_str(); // turning choice into a const char * for strcmp
+        //also want to make it so the input is lowercased. probably have to do that before making
+        // it a const char *
 
         if(strcmp(choice, warr) == 0){
-
-            //do stuff
+            Warrior *retWarr = createWarrior(name_create, 15, 15);
+            std::cout<< retWarr->attack<<std::endl;
+            break;
+            // don't know how to return, may need an abstract class
         }
-};
-    //create->name = name_create;
-    //create->health = 10;
-    //create->attack = 10;   
-    //std::cout<< create->name << std::endl;
-    //free(name_create)
+
+        if(strcmp(choice, mag) == 0){
+            Mage *retMag = createMage(name_create, 20, 10);
+            std::cout<< retMag->attack<<std::endl;
+            break;
+            // don't know how to return, may need an abstract class
+        }
+
+        if(strcmp(choice, pal) == 0){
+            Paladin *retPal = createPaladin(name_create, 10, 20);
+            std::cout << retPal->attack<<std::endl;
+            break;
+            // don't know how to return, may need an abstract class
+        }
+    };
+
 }
 
 
